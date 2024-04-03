c4f72a03aee44807c3a1e3c24068ead9cd836211
Malith Dilshan <79596630+Malith-19@users.noreply.github.com>
2024-04-03 23:12:50 +0530

    Add basic apis. (#5)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from env import SETTINGS
+import subprocess
+from constants import STATER_TEST
 
 app = FastAPI()
 
@@ -7,3 +9,19 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/tests")
+async def run_tests():
+    try:
+        script_path = SETTINGS.TEST_SUITE_PATH
+        subprocess.Popen(["sh", "-c", f"cd {script_path} && {STATER_TEST}"])
+        return {"message": "tests started."}
+    except Exception as e:
+        raise HTTPException(500, detail=str(e))
+
+
+@app.get("/container/status")
+async def container_status():
+    result = subprocess.run(["docker", "ps"], capture_output=True, text=True)
+    return {"output": result.stdout, "error": result.stderr}
