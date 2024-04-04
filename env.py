8875e0dc0f871d47188bbe2c28bd8fd8af76fb7e
Malith Dilshan <79596630+Malith-19@users.noreply.github.com>
2024-04-04 23:14:58 +0530

    Add container up & down scripts (#14)
    * Add jalien setup path to env.
    
    * Add container up and down functions.

--- env.py
+++ env.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     TEST_SUITE_PATH: str
+    JALIEN_SETUP_PATH: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- routes/container_route.py
+++ routes/container_route.py
@@ -8,3 +8,12 @@ router = APIRouter(prefix="/container", tags=["container"])
 async def container_status():
     return ContainerService().check_status()
 
+
+@router.get("/up")
+async def container_up():
+    return ContainerService().up_containers()
+
+
+@router.get("/down")
+async def container_down():
+    return ContainerService().down_containers()
