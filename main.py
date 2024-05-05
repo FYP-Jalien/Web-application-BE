3a4637491e0ee89e0f69ed50c6dc2b0de0b08661
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-05-05 14:16:46 +0530

    add cross origin support.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from routes import container_route, tests_route
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # Set to True if cookies are needed across domains
+    allow_methods=["*"],  # Allows all methods (GET, POST, PUT, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Adding routers to the app.
 app.include_router(container_route.router)
