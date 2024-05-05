71e74156e9908a2e00d7cb3eadc3209e10af1e2c
Malith-19 <malith.19@cse.mrt.ac.lk>
2024-05-05 21:05:02 +0530

    Add api to send the test summary.

--- routes/tests_route.py
+++ routes/tests_route.py
@@ -7,3 +7,13 @@ router = APIRouter(prefix="/tests", tags=["tests"])
 @router.get("/")
 async def run_tests():
     return TestService().start_tests()
+
+
+@router.get("/stop/{pid}")
+async def stop_tests(pid: int):
+    return TestService().stop_tests(pid)
+
+
+@router.get("/summary")
+async def read_summary():
+    return TestService().read_test_summary()
